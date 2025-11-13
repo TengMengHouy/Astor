f3dd134ff0e8d8afcd121cd5195f7accbe56c037
TengMengHouy <la9089975@gmail.com>
2025-11-13 10:53:14 +0700

    fix bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.get("token");
-    const protectedPaths = ["/product", "/cart", "/checkout"]; // protect these
+    const protectedPaths = [ "/cart", "/checkout"]; // protect these
 
     const pathname = request.nextUrl.pathname;
 
